cbd59391ce51ed78d51636f74630f963beca8ce0
Partha Khare <parthakhare1812@gmail.com>
2026-07-31 19:09:38 +0530

    Add solution for Koshary

---
+++ 2227A.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if ((x % 2 == 1) && (y % 2 == 1))
+                System.out.println("NO");
+            else
+                System.out.println("YES");
+        }
+
+        sc.close();
+    }
+}
