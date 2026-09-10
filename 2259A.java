f80d0403e27b01a95a1b2682837797156bf66da2
Partha Khare <parthakhare1812@gmail.com>
2026-09-10 07:40:45 +0530

    Add solution for Moo Language School

---
+++ 2259A.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            String s = sc.next();
+
+            int ans = 0;
+
+            for (int i = 0; i < n; i += k) {
+                boolean hasZero = false;
+
+                for (int j = i; j < i + k; j++) {
+                    if (s.charAt(j) == '0') {
+                        hasZero = true;
+                        break;
+                    }
+                }
+
+                if (!hasZero) {
+                    ans++;
+                }
+            }
+
+            System.out.println(ans);
+        }
+
+        sc.close();
+    }
+}
